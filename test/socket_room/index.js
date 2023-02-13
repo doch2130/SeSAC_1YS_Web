4ccbdb1a93ea5f91dbdcf6bd0eaa5d1bd3172811
HyoHyun_Home <doch2130@gmail.com>
2023-02-13 13:28:24 +0900

    feat: 2023-02-13

--- test/socket_room/index.js
+++ test/socket_room/index.js
@@ -3,89 +3,61 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+
+app.use("/public", express.static(__dirname+"/public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  // res.sendFile(__dirname + "/index.html");
+  res.render('index');
 });
 
-// 소켓아이디, 닉네임
-let list = {}
 
-// 클라이언트 소켓이 연결이 되면 콜백 함수가 실행된다.
-// socket: 클라이언트 소켓과 연결이 되고 새로 생성된 소켓이다.
 io.on('connection', (socket) => {
-  console.log('Server Socket Connected');
+  console.log('Socket Connected');
 
-  // 사용자 구분을 위해 클라이언트에게 socket.id 전달해주기
-  socket.emit('info', socket.id);
+  // 소켓 ID
+  console.log('socket.id', socket.id);
 
-  socket.emit('welcome', {msg: 'welcome'});
+  // 현재 소켓 접속 수
+  console.log(io.engine.clientsCount);
+  // socket.emit('usercount', io.engine.clientsCount);
 
+  // 기본 메시지 보내기 - 해당 socektID에게만 전송
+  socket.emit('socket welcome', {msg: 'socket welcome'});
+
+  // 기본 메시지 받아서 처리하기 - 해당 socektID에게 받은 메시지 처리
   socket.on('response', (str) => {
     console.log(str);
   });
 
+  // 연결되어 있는 전체 SocketID에 메시지 전달
+  io.emit('io welcome', {msg: 'io welcome'});
+
+  // room 237 방에 참가하기
+  socket.join("room 237");
+  
+  // Set { <socket.id>, "room 237" }
+  // Set(2) { 'Krx5QAPMzzSWpsqAAAAC', 'room 237' }
+  console.log(socket.rooms); 
+
+  // 동시에 여러방에 참가도 가능!
+  socket.join(["room 237", "room 238"]); 
+
+  // 해당 방의 사용자에게 메시지 보내기
+  io.to("room 237").emit("a new user has joined the room");
+
+  // 방을 나갈 때
+  socket.leave("room 237");
+  // 방 나갈 때 해당 방의 모든 사용자에게 메시지 전송
+  io.to("room 237").emit(`user ${socket.id} has left the room`);
+
+  // 사용자가 연결 끊었을 때
   socket.on('disconnect', () => {
     console.log('Server Socket disconnected');
   });
-
-
-  // 브로드캐스트로 서버에 접속된 모든 사용자에게 메시지를 전송한다.
-  // io.emit('notice', {msg: '익명님이 입장하셨습니다'});
-
-  // 사용자가 닉네임을 입력하고 들어오면 userName을 socketID와 mapping 시켜준다.
-  // 여기서는 json으로 저장
-  socket.on('userName', (name) => {
-    list[socket.id] = name;
-    io.emit('list', list);
-    io.emit('notice', {msg: name + '님이 입장하셨습니다.'});
-  });
-
-  // 다른 사용자 나가면 닉네임 + 퇴장 문구 출력
-  socket.on('disconnect', () => {
-    io.emit('notice', {msg: list[socket.id] + '님이 퇴장하셨습니다.'});
-    // 퇴장해도 list가 남아있으므로 삭제하는 작업을 해준다.
-    delete list[socket.id];
-    io.emit('list', list);
-  });
-
-  // 사용자가 보낸 메시지를 서버에서 받는다.
-  // 받은 다음에 다시 클라이언트로 뿌려준다.
-  socket.on('send', (data) => {
-    data['from'] = socket.id;
-    data['userName'] = list[socket.id];
-    data['isDM'] = false;
-    if(data.to === '전체') {
-      io.emit('newMSG', data);
-    } else {
-      data['isDM'] = true;
-      // console.log(Object.keys(list));
-      console.log(Object.keys(list).find(key => list[key] == data.to));
-      const socketID = Object.keys(list).find(key => list[key] == data.to);
-      // 받은 상대방한테만 메시지가 출력된다.
-      io.to(socketID).emit('newMSG', data);
-      // 나한테도 메시지가 출력되게 설정한다.
-      socket.emit('newMSG', data);
-    }
-  });
-
-
-
-
-  // 디버그 신호를 주고받는 네임스페이스
-  const debug = io.of('/debug');
-
-  // 네임스페이스의 연결 처리는 제각각이다. 그러므로 연결 콜백을 다시 만들어야 한다.
-  debug.on('connection', (socket) => {
-    // 룸의 목록 요청시 / 네임스페이스의 룸 목록 반환
-    socket.on('getRooms', () => { 
-      // 다른 네임스페이스의 객체에도 접근할 수 있다.
-      socket.emit('rooms', io.sockets.adapter.rooms);
-      console.log('rooms', io.sockets.adapter.rooms);
-  });
-});
-
 });
 
 http.listen(8080, () => {
